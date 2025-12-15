b6af1a3169297d81a611fea9b52d1d33f9b6079d
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 16:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ const table = document.querySelector("table")
 
 const getSum = () => {
 	let prices = document.querySelectorAll(".prices")
-	let sum = 0;
-	for(let price of prices) {
-	  sum+=parseFloat(price.textContent) || 0;
-	}
-	const existingTotalRow = document.querySelector('.total-price-row')
-	if(existingTotalRow) {
-		existingTotalRow.remove();
-	}
-	const newRow  = document.createElement("tr")
-	newRow.classList.add('total-price-row');
-	newRow.innerHTML = `<td>Total Price: ${sum}</td>`
-	table.appendChild(newRow)
+	let total = 0;
+	prices.forEach(price => {
+		total += Number(price.textContent)
+	})
+	const tr = document.createElement("tr")
+	const td = document.createElement("td")
+	td.colSpan = 2;
+	td.textContent = `Total Value: ${total}`
+	
+	tr.appendChild(td)
+	table.appendChild(tr)
 };
 
 getSumBtn.addEventListener("click", getSum)
