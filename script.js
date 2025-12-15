d9bb83f5f5a417fd4c8e5d20b36bc101b2d9357e
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 16:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ const getSum = () => {
 	let prices = document.querySelectorAll(".prices")
 	let total = 0;
 	prices.forEach(price => {
-		total += Number(price.textContent)
+		total += Number(price.textContent) || 0;
 	})
+	const existingTotal = document.getElementById("total-row")
+	if(existingTotal) {
+		existingTotal.remove()
+	}
+	
 	const tr = document.createElement("tr")
+	tr.id = "total-row"
+	
 	const td = document.createElement("td")
 	td.colSpan = 2;
 	td.textContent = `Total Value: ${total}`
@@ -17,5 +24,10 @@ const getSum = () => {
 	tr.appendChild(td)
 	table.appendChild(tr)
 };
-
 getSumBtn.addEventListener("click", getSum)
+
+
+
+
+
+
