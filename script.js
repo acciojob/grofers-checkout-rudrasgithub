604902eda8f51b112a92a373d0ed7511852e97bd
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2025-12-15 16:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const getSumBtn = document.createElement("button")
 getSumBtn.append("Get Total Price")
 document.body.appendChild(getSumBtn);
+const table = document.querySelector("table")
+const newRow  = document.createElement("tr")
 
 const getSum = () => {
 	let prices = document.querySelectorAll(".prices")
@@ -8,11 +10,7 @@ const getSum = () => {
 	for(let price of prices) {
 	  sum+=parseFloat(price.textContent) || 0;
 	}
-	const table = document.querySelector("table")
-	const newRow  = document.createElement("tr")
-	const newCell = document.createElement("td")
-	newCell.textContent = `Total Price: ${sum}`
-	newRow.appendChild(newCell)
+	newRow.innerHTML = `<td>Total Price: ${sum}</td>`
 	table.appendChild(newRow)
 };
 
